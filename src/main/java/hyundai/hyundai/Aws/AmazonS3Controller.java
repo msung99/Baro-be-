59f76adc2e971bfa86d1b9d3b6da286a5c1dff17
msung99 <msung6924@naver.com>
2023-02-10 23:45:00 +0900

    docs: AWS S3 Controller Swagger 명세

--- src/main/java/hyundai/hyundai/Aws/AmazonS3Controller.java
+++ src/main/java/hyundai/hyundai/Aws/AmazonS3Controller.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +19,17 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/file")
+@Api(value = "file", tags = "AWS 파일 관리 API")
 public class AmazonS3Controller {
     private final AwsS3Service awsS3Service;
     @PostMapping("/uploadFile")
+    @Operation(summary = "파일 업로드", description = "MultipartFiles 을 리스트 형태로 입력")
     public ResponseEntity<List<String>> uploadFile(List<MultipartFile> multipartFiles){
          return ResponseEntity.ok(awsS3Service.uploadFile(multipartFiles));
     }
 
     @DeleteMapping("/deleteFile")
+    @Operation(summary = "파일 삭제", description = "fileName 에 해당하는 파일 삭제")
     public ResponseEntity<String> deleteFile(@RequestParam String fileName){
         awsS3Service.deleteFile(fileName);
         return ResponseEntity.ok(fileName);
