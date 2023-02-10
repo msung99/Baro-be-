29bb72edb4ff66d5f9f6eaefa474f08eee64206b
msung99 <msung6924@naver.com>
2023-02-10 23:53:29 +0900

    feat: Cors Settings

---
+++ src/main/java/hyundai/hyundai/Config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package hyundai.hyundai.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
