a3cffdf018d678d2f1c74dac36ea15a8dd2e5d2c
msung99 <msung6924@naver.com>
2023-02-12 15:33:17 +0900

    docs: 로그인 API Swagger 명세 추가

--- src/main/java/hyundai/hyundai/User/model/LoginUserReq.java
+++ src/main/java/hyundai/hyundai/User/model/LoginUserReq.java
@@ -1,5 +1,6 @@
 package hyundai.hyundai.User.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginUserReq {
+
+    @ApiModelProperty(example = "msung99@naver.com")
     private String email;
+
+    @ApiModelProperty(example = "mypassword")
     private String password;
 }
--- src/main/java/hyundai/hyundai/User/model/LoginUserRes.java
+++ src/main/java/hyundai/hyundai/User/model/LoginUserRes.java
@@ -1,5 +1,6 @@
 package hyundai.hyundai.User.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginUserRes {
+
+    @ApiModelProperty(example = "3")
     int userIdx;
 }
