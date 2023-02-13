99303e0053ca2140b41bc651d8414ac842e3a6d0
msung99 <msung6924@naver.com>
2023-02-13 13:45:03 +0900

    refactor: 회원가입 Pattern 검증 리팩토링 및 UserEntity 수정

--- src/main/java/hyundai/hyundai/User/model/UserEntity.java
+++ src/main/java/hyundai/hyundai/User/model/UserEntity.java
@@ -17,7 +17,7 @@ public class UserEntity {
     // @Column 을 사용할경우 객체의 필드와 테이블의 컬럼을 매핑한다.
     // 만일 @Column 을 생략할경우 필드의 이름을 테이블 컬럼 이름으로 사용한다.
     @Column(unique = true)
-    private String email;
+    private String identification;  // 아이디
     private String nickname;
     private String password;
 
