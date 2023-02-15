3b3c49dd353f6953ec84b4e012bbdec197c9b051
msung99 <msung6924@naver.com>
2023-02-15 16:07:15 +0900

    fix: Regex 회원가입 검증처리 버그수정

--- src/main/java/hyundai/hyundai/User/model/IdentificationReq.java
+++ src/main/java/hyundai/hyundai/User/model/IdentificationReq.java
@@ -14,7 +14,7 @@ public class IdentificationReq {
 
     @ApiModelProperty(example = "msung1234")
     @NotEmpty(message = "아이디는 필수 입력값입니다")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "^[a-z0-9]{5,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 5~20자리여야 합니다.")
     String identification;
     //     @Pattern(regexp = "^[a-z0-9]{5,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 5~20자리여야 합니다.")
 
